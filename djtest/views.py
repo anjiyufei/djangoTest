528de8c16d5a2fb1a53ad0278a7d49d98bb25100
anjiyufei <anjiyufei@gmail.com>
2019-01-16 17:01:51 +0800

    增加跳转等功能

--- djtest/views.py
+++ djtest/views.py
@@ -2,11 +2,35 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.http import JsonResponse
+from django.shortcuts import redirect
 
 def home(request):
     return render(request,'home.html')
 
-#add.html?a=4&b=5
+
+def sum(request):
+    a = request.GET.get('a',0)
+    b = request.GET.get('b',0)
+    c = int(a) + int(b)
+    return HttpResponse(str(c))
+
+def sum2(request):
+    a = request.GET.get('a',0)
+    b = request.GET.get('b',0)
+    c = int(a) + int(b)
+    context = {'c':c}
+    #context['c'] = str(c)
+    return render(request,'sum.html',context)
+
+def login2(request):
+    return redirect('login.html?err=2')
+
+def login(request):
+    err = request.GET.get('err', 0)
+    context = {'err':err}
+    #context['err'] = str(err)
+    return render(request, 'login.html', context)
+
 def add(request):
     return render(request, 'add.html')
 
